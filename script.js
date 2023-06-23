ac61ac4aacfa270df6a8cc7e5556fa6c42e12e50
AceMentor4 <136705510+AceMentor4@users.noreply.github.com>
2023-06-23 23:01:35 +0500

    index.html , script.js , style.css addes

---
+++ script.js
@@ -0,0 +1,14 @@
+function clearResult() {
+    document.getElementById("result").value = "";
+  }
+  
+  function appendToResult(value) {
+    document.getElementById("result").value += value;
+  }
+  
+  function calculate() {
+    var result = document.getElementById("result").value;
+    var answer = eval(result);
+    document.getElementById("result").value = answer;
+  }
+  
